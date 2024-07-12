eb40b13c28a83e941178575b69f635a3110a031a
MochFarrel06 <mochfarrel2606@gmail.com>
2024-07-12 22:09:49 +0700

    first commit

---
+++ database/migrations/2024_07_12_144156_create_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('item_type_id');
+            $table->unsignedBigInteger('unit_type_id');
+
+            $table->string('name');
+            $table->string('item_code');
+            $table->bigInteger('stock')->default(0);
+            $table->bigInteger('reorder_level');
+            $table->decimal('price', 8, 2);
+            $table->string('photo');
+
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('item_type_id')->references('id')->on('item_types')->onDelete('cascade');
+            $table->foreign('unit_type_id')->references('id')->on('unit_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
