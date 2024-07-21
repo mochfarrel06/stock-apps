ab90c54c6e80fd7b1b37a0f4a967c92a852ae17c
MochFarrel06 <mochfarrel2606@gmail.com>
2024-07-21 22:10:31 +0700

    Add outgoing item CRUD to admin

--- app/Http/Controllers/Admin/OutgoingItem/OutgoingItemController.php
+++ app/Http/Controllers/Admin/OutgoingItem/OutgoingItemController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin\OutgoingItem;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\OutgoingItem\OutgoingItemCreateRequest;
+use App\Models\Item;
+use App\Models\OutgoingItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OutgoingItemController extends Controller
 {
@@ -12,7 +16,9 @@ class OutgoingItemController extends Controller
      */
     public function index()
     {
-        //
+        $outgoingItems = OutgoingItem::all();
+
+        return view('administrator.outgoing-item.index', compact('outgoingItems'));
     }
 
     /**
@@ -20,15 +26,40 @@ class OutgoingItemController extends Controller
      */
     public function create()
     {
-        //
+        $items = Item::all();
+
+        return view('administrator.outgoing-item.create', compact('items'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(OutgoingItemCreateRequest $request)
     {
-        //
+        try {
+            $userId = Auth::id();
+            $item = Item::findOrFail($request->item_id);
+
+            $quantity = $request->quantity;
+
+            $outgoingItem = new OutgoingItem([
+                'user_id' => $userId,
+                'item_id' => $item->id,
+                'quantity' => $quantity
+            ]);
+
+            $outgoingItem->save();
+
+            $item->stock -= $quantity;
+            $item->save();
+
+            session()->flash('success', 'Berhasil menambahkan data barang keluar');
+            return response()->json(['success' => true], 200);
+        } catch (\Exception $e) {
+            // Log error dan tampilkan pesan error
+            session()->flash('error', 'Terdapat kesalahan pada proses data barang keluar: ' . $e->getMessage());
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
     }
 
     /**
@@ -36,7 +67,10 @@ class OutgoingItemController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $outgoingItem = OutgoingItem::findOrFail($id);
+        $item = $outgoingItem->item;
+
+        return view('administrator.outgoing-item.show', compact('outgoingItem', 'item'));
     }
 
     /**
@@ -60,6 +94,22 @@ class OutgoingItemController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $outgoingItem = OutgoingItem::findOrFail($id);
+            $item = $outgoingItem->item;
+
+            // Kurangi stok barang sesuai quantity dari barang masuk yang dihapus
+            $item->stock += $outgoingItem->quantity;
+            $item->save();
+
+            // Hapus data barang masuk
+            $outgoingItem->delete();
+
+            // Memberikan respons bahwa penghapusan berhasil
+            return response(['status' => 'success', 'message' => 'Berhasil menghapus barang keluar']);
+        } catch (\Exception $e) {
+            // Menangani exception jika terjadi kesalahan saat menghapus
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
     }
 }
