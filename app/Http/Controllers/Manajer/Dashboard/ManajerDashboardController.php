b40c720cd2dd23396b0a14010628d33a0aa2e2d9
MochFarrel06 <mochfarrel2606@gmail.com>
2024-07-30 19:38:32 +0700

    Modify dashboard page

--- app/Http/Controllers/Manajer/Dashboard/ManajerDashboardController.php
+++ app/Http/Controllers/Manajer/Dashboard/ManajerDashboardController.php
@@ -8,52 +8,29 @@ use App\Models\Item;
 use App\Models\ItemType;
 use App\Models\OutgoingItem;
 use App\Models\UnitType;
+use App\Repositories\Contracts\DashboardRepositoryInterface;
 
 class ManajerDashboardController extends Controller
 {
+    protected $repository;
+
+    public function __construct(DashboardRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
-        $itemCount = Item::all()->count();
-        $incomingCount = IncomingItem::all()->count();
-        $outgoingCount = OutgoingItem::all()->count();
-        $itemType = ItemType::all()->count();
-        $unitType = UnitType::all()->count();
-
-        $items = Item::whereColumn('stock', '<=', 'reorder_level')->get();
+        $data = $this->repository->getDashboard();
 
         $cards = [
-            [
-                'title' => 'Data Barang',
-                'bg_color' => 'primary',
-                'value' => $itemCount,
-                'icon' => 'fas fa-box'
-            ],
-            [
-                'title' => 'Barang Masuk',
-                'bg_color' => 'success',
-                'value' => $incomingCount,
-                'icon' => 'fas fa-download'
-            ],
-            [
-                'title' => 'Barang Keluar',
-                'bg_color' => 'danger',
-                'value' => $outgoingCount,
-                'icon' => 'fas fa-upload'
-            ],
-            [
-                'title' => 'Jenis Barang',
-                'bg_color' => 'info',
-                'value' => $itemType,
-                'icon' => 'fas fa-cube'
-            ],
-            [
-                'title' => 'Satuan Barang',
-                'bg_color' => 'warning',
-                'value' => $unitType,
-                'icon' => 'fas fa-folder'
-            ]
+            ['title' => 'Data Barang', 'bg_color' => 'primary', 'value' => $data['itemCount'], 'icon' => 'fas fa-box'],
+            ['title' => 'Barang Masuk', 'bg_color' => 'success', 'value' => $data['incomingCount'], 'icon' => 'fas fa-download'],
+            ['title' => 'Barang Keluar', 'bg_color' => 'danger', 'value' => $data['outgoingCount'], 'icon' => 'fas fa-upload'],
+            ['title' => 'Jenis Barang', 'bg_color' => 'info', 'value' => $data['itemType'], 'icon' => 'fas fa-cube'],
+            ['title' => 'Satuan Barang', 'bg_color' => 'warning', 'value' => $data['unitType'], 'icon' => 'fas fa-folder']
         ];
 
-        return view('manajer.dashboard.index', compact('cards', 'items'));
+        return view('manajer.dashboard.index', ['cards' => $cards, 'data' => $data['lowStockItems']]);
     }
 }
