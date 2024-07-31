cce37fbae3edc38e44f2a84739e3063ab8d54b79
MochFarrel06 <mochfarrel2606@gmail.com>
2024-07-31 19:28:24 +0700

    Modify the admin dashboard page to make it cleaner

--- app/Http/Controllers/Admin/Dashboard/AdminDashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/AdminDashboardController.php
@@ -3,80 +3,32 @@
 namespace App\Http\Controllers\Admin\Dashboard;
 
 use App\Http\Controllers\Controller;
-use App\Models\IncomingItem;
-use App\Models\Item;
-use App\Models\ItemType;
-use App\Models\OutgoingItem;
-use App\Models\UnitType;
-use App\Models\User;
-use Illuminate\Http\Request;
+use App\Repositories\Contracts\DashboardRepositoryInterface;
 
 class AdminDashboardController extends Controller
 {
+    protected $repository;
+
+    public function __construct(DashboardRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
-        $itemCount = Item::all()->count();
-        $incomingCount = IncomingItem::all()->count();
-        $outgoingCount = OutgoingItem::all()->count();
-        $itemType = ItemType::all()->count();
-        $unitType = UnitType::all()->count();
-        $userGudangCount = User::where('role', '=', 'Gudang')->count();
-        $userManajerCount = User::where('role', '=', 'Manajer')->count();
-        $userAdminCount = User::where('role', '=', 'Administrator')->count();
-
-        $items = Item::whereColumn('stock', '<=', 'reorder_level')->get();
+        $data = $this->repository->getDashboard();
 
         $cards = [
-            [
-                'title' => 'Pengguna Gudang',
-                'bg_color' => 'primary',
-                'value' => $userGudangCount,
-                'icon' => 'fas fa-solid fa-user'
-            ],
-            [
-                'title' => 'Pengguna Manajer',
-                'bg_color' => 'success',
-                'value' => $userManajerCount,
-                'icon' => 'fas fa-solid fa-user'
-            ],
-            [
-                'title' => 'Pengguna Admin',
-                'bg_color' => 'warning',
-                'value' => $userAdminCount,
-                'icon' => 'fas fa-solid fa-user'
-            ],
-            [
-                'title' => 'Data Barang',
-                'bg_color' => 'primary',
-                'value' => $itemCount,
-                'icon' => 'fas fa-box'
-            ],
-            [
-                'title' => 'Barang Masuk',
-                'bg_color' => 'success',
-                'value' => $incomingCount,
-                'icon' => 'fas fa-download'
-            ],
-            [
-                'title' => 'Barang Keluar',
-                'bg_color' => 'danger',
-                'value' => $outgoingCount,
-                'icon' => 'fas fa-upload'
-            ],
-            [
-                'title' => 'Jenis Barang',
-                'bg_color' => 'info',
-                'value' => $itemType,
-                'icon' => 'fas fa-cube'
-            ],
-            [
-                'title' => 'Satuan Barang',
-                'bg_color' => 'warning',
-                'value' => $unitType,
-                'icon' => 'fas fa-folder'
-            ]
+            ['title' => 'Pengguna Gudang', 'bg_color' => 'warning', 'value' => $data['userGudangCount'], 'icon' => 'fas fa-user'],
+            ['title' => 'Pengguna Manajer', 'bg_color' => 'info', 'value' => $data['userManajerCount'], 'icon' => 'fas fa-user'],
+            ['title' => 'Pengguna Admin', 'bg_color' => 'danger', 'value' => $data['userAdminCount'], 'icon' => 'fas fa-user'],
+            ['title' => 'Data Barang', 'bg_color' => 'primary', 'value' => $data['itemCount'], 'icon' => 'fas fa-box'],
+            ['title' => 'Barang Masuk', 'bg_color' => 'success', 'value' => $data['incomingCount'], 'icon' => 'fas fa-download'],
+            ['title' => 'Barang Keluar', 'bg_color' => 'danger', 'value' => $data['outgoingCount'], 'icon' => 'fas fa-upload'],
+            ['title' => 'Jenis Barang', 'bg_color' => 'info', 'value' => $data['itemType'], 'icon' => 'fas fa-cube'],
+            ['title' => 'Satuan Barang', 'bg_color' => 'warning', 'value' => $data['unitType'], 'icon' => 'fas fa-folder']
         ];
 
-        return view('administrator.dashboard.index', compact('cards', 'items'));
+        return view('administrator.dashboard.index', ['cards' => $cards, 'data' => $data['lowStockItems']]);
     }
 }
