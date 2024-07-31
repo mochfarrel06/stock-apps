d6f5175cfe98986fa43891609bb5acfa84537a22
MochFarrel06 <mochfarrel2606@gmail.com>
2024-07-31 21:07:02 +0700

    modify the layout from admin item

--- resources/views/administrator/item/create.blade.php
+++ resources/views/administrator/item/create.blade.php
@@ -5,112 +5,99 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
+    <x-content.container-fluid>
 
-        <!-- Page Heading -->
-        <div class="d-lg-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 mt-2 text-gray-900">Tambah Data Barang</h1>
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb mb-0 mt-2">
-                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                    <li class="breadcrumb-item"><a href="{{ route('admin.item.index') }}">Data Barang</a></li>
-                    <li class="breadcrumb-item">Tambah</li>
-                </ol>
-            </nav>
-        </div>
+        <x-content.heading-page :title="'Tambah Data Barang'" :breadcrumbs="[
+            ['title' => 'Dashboard', 'url' => route('admin.dashboard')],
+            ['title' => 'Data Barang', 'url' => route('admin.item.index')],
+            ['title' => 'Tambah'],
+        ]" />
 
-        <div class="row">
-            <div class="col-lg-12">
-                <!-- Basic Card Example -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Tambah Data Barang</h6>
-                    </div>
-                    <div class="card-body">
-                        <form id="main-form" action="{{ route('admin.item.store') }}" method="POST"
-                            enctype="multipart/form-data">
-                            @csrf
+        <x-content.table-container>
 
-                            <div class="row">
-                                <div class="col-lg-7">
-                                    <div class="form-group">
-                                        <label for="item_code">Kode Barang</label>
-                                        <input type="text" class="form-control @error('item_code') is-invalid @enderror"
-                                            name="item_code" id="item_code" value="{{ old('item_code') }}"
-                                            placeholder="Masukkan Kode Barang">
-                                    </div>
+            <x-content.table-header :title="'Tambah Data Barang'" :icon="'fas fa-solid fa-plus'" />
 
-                                    <div class="form-group">
-                                        <label for="name">Nama Barang</label>
-                                        <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                            name="name" id="name" value="{{ old('name') }}"
-                                            placeholder="Masukkan Nama Barang">
-                                    </div>
+            <x-content.card-body>
+                <form id="main-form" action="{{ route('admin.item.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
 
-                                    <div class="form-group">
-                                        <label for="item_type_id">Jenis Barang</label>
-                                        <select class="form-control @error('item_type_id') is-invalid @enderror"
-                                            name="item_type_id" id="item_type_id">
-                                            <option value="">-- Pilih Jenis Barang --</option>
-                                            @foreach ($itemTypes as $itemType)
-                                                <option value="{{ $itemType->id }}">{{ $itemType->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="unit_type_id">Satuan Barang</label>
-                                        <select class="form-control @error('unit_type_id') is-invalid @enderror"
-                                            name="unit_type_id" id="unit_type_id">
-                                            <option value="">-- Pilih Satuan Barang --</option>
-                                            @foreach ($unitTypes as $unitType)
-                                                <option value="{{ $unitType->id }}">{{ $unitType->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="reorder_level">Stock Barang Minimum*</label>
-                                        <input type="number"
-                                            class="form-control @error('reorder_level') is-invalid @enderror"
-                                            name="reorder_level" id="reorder_level" value="{{ old('reorder_level') }}"
-                                            placeholder="Masukkan Stock Barang Minimum">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="price">Harga Barang</label>
-                                        <input type="number" class="form-control @error('price') is-invalid @enderror"
-                                            name="price" id="price" value="{{ old('price') }}"
-                                            placeholder="Masukkan Harga Barang">
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-5">
-                                    <div class="form-group">
-                                        <label for="photo" class="form-label">Gambar Produk</label>
-                                        <div class="image-upload-wrapper">
-                                            <input class="form-control" type="file" id="photo" name="photo"
-                                                onchange="previewImage(event)">
-                                            <div class="image-upload-text" id="upload-text">
-                                                Choose File
-                                            </div>
-                                            <div class="preview-image mt-3">
-                                                <img id="preview" src="#" alt="Gambar Produk"
-                                                    style="display: none;">
-                                            </div>
-                                        </div>
-                                        <div class="text-info mt-2">*File harus berformat JPG, JPEG, PNG</div>
-                                        <div class="text-info">*File harus berukuran 1000 KB</div>
-                                    </div>
-                                </div>
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="form-group">
+                                <label for="item_code">Kode Barang</label>
+                                <input type="text" class="form-control @error('item_code') is-invalid @enderror"
+                                    name="item_code" id="item_code" value="{{ old('item_code') }}"
+                                    placeholder="Masukkan Kode Barang">
                             </div>
 
-                            <button type="submit" id="submit-btn" class="btn btn-primary mt-3">Tambah</button>
-                            <a href="{{ route('admin.item.index') }}" class="btn btn-warning mt-3 ml-2">Kembali</a>
-                        </form>
+                            <div class="form-group">
+                                <label for="name">Nama Barang</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                    name="name" id="name" value="{{ old('name') }}"
+                                    placeholder="Masukkan Nama Barang">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="item_type_id">Jenis Barang</label>
+                                <select class="form-control @error('item_type_id') is-invalid @enderror" name="item_type_id"
+                                    id="item_type_id">
+                                    <option value="">-- Pilih Jenis Barang --</option>
+                                    @foreach ($itemTypes as $itemType)
+                                        <option value="{{ $itemType->id }}">{{ $itemType->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="unit_type_id">Satuan Barang</label>
+                                <select class="form-control @error('unit_type_id') is-invalid @enderror" name="unit_type_id"
+                                    id="unit_type_id">
+                                    <option value="">-- Pilih Satuan Barang --</option>
+                                    @foreach ($unitTypes as $unitType)
+                                        <option value="{{ $unitType->id }}">{{ $unitType->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="reorder_level">Stock Barang Minimum*</label>
+                                <input type="number" class="form-control @error('reorder_level') is-invalid @enderror"
+                                    name="reorder_level" id="reorder_level" value="{{ old('reorder_level') }}"
+                                    placeholder="Masukkan Stock Barang Minimum">
+                            </div>
+                            <div class="form-group">
+                                <label for="price">Harga Barang</label>
+                                <input type="number" class="form-control @error('price') is-invalid @enderror"
+                                    name="price" id="price" value="{{ old('price') }}"
+                                    placeholder="Masukkan Harga Barang">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-5">
+                            <div class="form-group">
+                                <label for="photo" class="form-label">Gambar Produk</label>
+                                <div class="image-upload-wrapper">
+                                    <input class="form-control @error('item_code') is-invalid @enderror" type="file"
+                                        id="photo" name="photo" onchange="previewImage(event)">
+                                    <div class="image-upload-text" id="upload-text">Choose File</div>
+                                    <div class="preview-image mt-3">
+                                        <img id="preview" src="#" alt="Gambar Produk" style="display: none;">
+                                    </div>
+                                    <div id="error-message" class="text-danger mt-2" style="display: none;"></div>
+                                </div>
+                                <div class="text-info mt-2">*File harus berformat JPG, JPEG, PNG</div>
+                                <div class="text-info">*File harus berukuran 1000 KB</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+                    <button type="submit" id="submit-btn" class="btn btn-primary mt-3">Tambah</button>
+                    <a href="{{ route('admin.item.index') }}" class="btn btn-warning mt-3 ml-2">Kembali</a>
+                </form>
+            </x-content.card-body>
+
+        </x-content.table-container>
+
+    </x-content.container-fluid>
 @endsection
 
 @push('scripts')
@@ -122,6 +109,30 @@
 
                 const form = $(this)[0];
                 const formData = new FormData(form); // Create FormData object with form data
+
+                // Validasi ukuran dan format file
+                const file = $('#photo')[0].files[0];
+                const errorMessage = $('#error-message');
+                let valid = true;
+
+                if (file) {
+                    if (file.size > 1024 * 1024) {
+                        errorMessage.text('*File harus berukuran maksimal 1000 KB');
+                        errorMessage.show();
+                        valid = false;
+                    }
+
+                    const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+                    if (!validImageTypes.includes(file.type)) {
+                        errorMessage.text('*File harus berformat JPG, JPEG, PNG');
+                        errorMessage.show();
+                        valid = false;
+                    }
+                }
+
+                if (!valid) {
+                    return;
+                }
 
                 submitBtn.prop('disabled', true).text('Loading...');
 
@@ -174,38 +185,16 @@
         });
 
         function previewImage(event) {
-            var file = event.target.files[0];
-            var reader = new FileReader();
-            var output = document.getElementById('preview');
-            var uploadText = document.getElementById('upload-text');
-            var errorMessage = document.getElementById('error-message');
-
-            // Validasi ukuran file (maks 1MB)
-            if (file.size > 1024 * 1024) {
-                errorMessage.textContent = '*File harus berukuran maksimal 1000 KB';
-                errorMessage.style.display = 'block';
-                output.style.display = 'none';
-                uploadText.style.display = 'none';
-                return;
-            }
-
-            // Validasi format file
-            var validImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-            if (!validImageTypes.includes(file.type)) {
-                errorMessage.textContent = '*File harus berformat JPG, JPEG, PNG';
-                errorMessage.style.display = 'block';
-                output.style.display = 'none';
-                uploadText.style.display = 'none';
-                return;
-            }
+            let reader = new FileReader();
+            let output = document.getElementById('preview');
+            let uploadText = document.getElementById('upload-text');
 
             reader.onload = function() {
                 output.src = reader.result;
                 output.style.display = 'block';
-                uploadText.style.display = 'none'; // Hide the "Choose File" text
-                errorMessage.style.display = 'none'; // Hide error message
+                uploadText.style.display = 'none'; // Sembunyikan teks "Choose File"
             };
-            reader.readAsDataURL(file);
+            reader.readAsDataURL(event.target.files[0]);
         }
     </script>
 @endpush
