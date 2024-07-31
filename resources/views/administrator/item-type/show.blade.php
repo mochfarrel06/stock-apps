900eb136734258500ea2015f3645710e1aedb642
MochFarrel06 <mochfarrel2606@gmail.com>
2024-07-31 21:16:11 +0700

    modify the layout from admin type item

--- resources/views/administrator/item-type/show.blade.php
+++ resources/views/administrator/item-type/show.blade.php
@@ -5,38 +5,30 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <!-- Page Heading -->
-        <div class="d-lg-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 mt-2 text-gray-900">Lihat Jenis Barang</h1>
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb mb-0 mt-2">
-                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                    <li class="breadcrumb-item"><a href="{{ route('admin.item-type.index') }}">Jenis Barang</a></li>
-                    <li class="breadcrumb-item">Lihat</li>
-                </ol>
-            </nav>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <!-- Basic Card Example -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Lihat Jenis Barang</h6>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            @csrf
+    <x-content.container-fluid>
 
-                            <div class="form-group">
-                                <label>Jenis Barang</label>
-                                <input type="text" class="form-control" value="{{ $itemType->name }}" disabled>
-                            </div>
-                            <a href="{{ route('admin.item-type.index') }}" class="btn btn-warning mt-3">Kembali</a>
-                        </form>
+        <x-content.heading-page :title="'Lihat Jenis Barang'" :breadcrumbs="[
+            ['title' => 'Dashboard', 'url' => route('admin.dashboard')],
+            ['title' => 'Jenis Barang', 'url' => route('admin.item-type.index')],
+            ['title' => 'Lihat'],
+        ]" />
+
+        <x-content.table-container>
+
+            <x-content.table-header :title="'Lihat Jenis Barang'" :icon="'fas fa-solid fa-eye'" />
+
+            <div class="card-body">
+                <form>
+                    @csrf
+
+                    <div class="form-group">
+                        <label>Jenis Barang</label>
+                        <input type="text" class="form-control" value="{{ $itemType->name }}" disabled>
                     </div>
-                </div>
+                    <a href="{{ route('admin.item-type.index') }}" class="btn btn-warning mt-3">Kembali</a>
+                </form>
             </div>
-        </div>
-    </div>
+        </x-content.table-container>
+
+    </x-content.container-fluid>
 @endsection
